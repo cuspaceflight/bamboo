6e7ddda274aae8c7344945030d08cafafb704afb
dug20 <daniel.u.gibbons@gmail.com>
2021-02-16 21:33:27 +0000

    Added setup.py so you can install it as a module with pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Allows installation via pip, e.g. by navigating to this directory with the command prompt, and using 'pip install .'
+"""
+
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name='bamboo',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy', 'matplotlib', 'scipy', 'ambiance'],
+    description='1D isentropic nozzle flow for perfect gases',
+)
