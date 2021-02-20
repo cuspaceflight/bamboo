6fb7cc40cf1d3bcb195352a6b8a544f7ff20cde7
dug20 <daniel.u.gibbons@gmail.com>
2021-02-20 10:08:49 +0000

    Cooling simulation runs, but produces questionable results
    Example available in cooling_example.py

--- bamboo/cooling.py
+++ bamboo/cooling.py
@@ -30,10 +30,11 @@ def black_body(T):
 
 '''Classes'''
 class EngineGeometry:
-    def __init__(self, combustion_chamber, nozzle, chamber_length, geometry="auto"):
+    def __init__(self, combustion_chamber, nozzle, chamber_length, wall_thickness, geometry="auto"):
         self.combustion_chamber = combustion_chamber
         self.nozzle = nozzle
         self.chamber_length = chamber_length
+        self.wall_thickness = wall_thickness
         self.geometry = geometry
 
 
@@ -100,16 +101,21 @@ class EngineGeometry:
 
 
 class CoolingJacket:
-    def __init__(self, channel_width, channel_height, channel_shape = "rectangle", configuration = "spiral"):
-        self.w = channel_width
-        self.h = channel_height
+    def __init__(self, k_wall, channel_width, channel_height, inlet_T, inlet_p0, coolant, mdot_coolant, channel_shape = "rectangle", configuration = "spiral"):
+        self.k_wall = k_wall
+        self.channel_width = channel_width
+        self.channel_height = channel_height
+        self.coolant = coolant          #thermo library Chemical
+        self.mdot_coolant = mdot_coolant
+        self.inlet_T = inlet_T
+        self.inlet_p0 = inlet_p0
         self.channel_shape = channel_shape
         self.configuration = configuration
         
         if self.channel_shape == "rectangle":
             #Page 317 of RPE 7th Edition
-            self.perimeter = 2*self.w + 2*self.h
-            self.flow_area = self.w*self.h
+            self.perimeter = 2*channel_width + 2*channel_height
+            self.flow_area = channel_width*channel_height
             self.hydraulic_radius = self.flow_area/self.perimeter
             self.equivelant_diameter = 4*self.hydraulic_radius
 
@@ -119,14 +125,15 @@ class CoolingJacket:
     def D(self, x=None):
         return self.equivelant_diameter
 
-
 class EngineWithCooling:
-    def __init__(self, engine_geometry, cooling_jacket, coolant, gas):
+    def __init__(self, engine_geometry, cooling_jacket, gas, thermo_gas):
         self.geometry = engine_geometry
         self.cooling_jacket = cooling_jacket
         self.gas = gas
+        self.thermo_gas = thermo_gas
 
     def M(self, x):
+        #Exhaust gas Mach number
         #If we're at the throat M=1 by default:
         if x==0:
             return 1.00
@@ -144,6 +151,7 @@ class EngineWithCooling:
             return Mach
 
     def T(self, x):
+        #Exhaust gas temperature
         return bam.T(self.geometry.combustion_chamber.T0, self.M(x), self.gas.gamma)
 
     def p(self, x):
@@ -204,7 +212,7 @@ class EngineWithCooling:
 
     def coolant_velocity(self, x, rho):
         #rho is the coolant density here!
-        return self.geometry.combustion_chamber.mdot/(rho * self.cooling_jacket.A(x))
+        return self.cooling_jacket.mdot_coolant/(rho * self.cooling_jacket.A(x))
 
     def h_gas(self, x, mu, k, Pr):
         """Get the convective heat transfer coefficient on the gas side.
@@ -237,3 +245,91 @@ class EngineWithCooling:
             float: Coolant side convective heat transfer coefficient
         """
         return 0.023*c_bar*(self.geometry.combustion_chamber.mdot/self.cooling_jacket.A(x))*(self.cooling_jacket.D(x)*self.coolant_velocity(x, rho)*rho/mu)**(-0.2)*(mu*c_bar/k)**(-2/3) 
+
+    def thermal_circuit(self, x, h_gas, h_coolant, k_wall, T_gas, T_coolant):
+        """
+        q is per unit length along the nozzle wall (axially) - positive when heat is flowing to the coolant.
+        Uses the idea of thermal circuits and resistances - we have three resistors in series.
+
+        Args:
+            x (float): x position (m)
+            h_gas (float): Gas side convective heat transfer coefficient
+            h_coolant (float): Coolant side convective heat transfer coefficient
+            k_wall (float): Wall thermal conductivity
+            T_gas (float): Free stream gas temperature (K)
+            T_coolant (float): Coolant temperature (K)
+
+        Returns:
+            float, float, float, float: q_dot, R_gas, R_wall, R_coolant
+        """
+
+        r = self.geometry.y(x)
+        
+        r_out = r + self.geometry.wall_thickness
+        r_in = r - self.geometry.wall_thickness
+
+        A_in = 2*np.pi*r_out    #Inner area per unit length (i.e. just the inner circumference)
+        A_out = 2*np.pi*r_in    #Outer area per unit length (i.e. just the outer circumference)
+
+        R_gas = 1/(h_gas*A_in)
+        R_wall = np.log(r_out/r_in)/(2*np.pi*k_wall)
+        R_coolant = 1/(h_coolant*A_out)
+
+        q_dot = (T_gas - T_coolant)/(R_gas + R_wall + R_coolant)
+
+        return q_dot, R_gas, R_wall, R_coolant
+
+    def run_heating_analysis(self, number_of_points=1000):
+        discretised_x = np.linspace(self.geometry.x_min, self.geometry.x_max, number_of_points)
+        dx = discretised_x[1] - discretised_x[0]
+
+        T_wall_inner = np.zeros(len(discretised_x))
+        T_wall_outer = np.zeros(len(discretised_x))
+        T_coolant = np.zeros(len(discretised_x))
+        T_gas = np.zeros(len(discretised_x))
+        q_dot = np.zeros(len(discretised_x))
+
+        coolant = self.cooling_jacket.coolant
+        exhaust_gas = self.thermo_gas
+
+        for i in range(len(discretised_x)):
+            x = discretised_x[i]
+            
+            if i == 0:
+                T_gas[i] = self.T(x)
+                p_gas = self.p(x)
+                T_coolant[i] = self.cooling_jacket.inlet_T
+
+                coolant.calculate(T = T_coolant[i], P = self.cooling_jacket.inlet_p0)
+                exhaust_gas.calculate(T = T_gas[i], P = p_gas)
+
+                h_gas = self.h_gas(x, exhaust_gas.mu, exhaust_gas.k, exhaust_gas.Pr)
+                h_coolant = self.h_coolant(x, coolant.mu, coolant.k, coolant.Cp, coolant.rho)
+
+                q_dot[i], R_gas, R_wall, R_coolant = self.thermal_circuit(x, h_gas, h_coolant, self.cooling_jacket.k_wall, T_gas[i], T_coolant[i])
+
+                T_wall_inner[i] = T_gas[i] - q_dot[i]*R_gas
+                T_wall_outer[i] = T_wall_inner[i] - q_dot[i]*R_wall
+
+            else:    
+                T_gas[i] = self.T(x)
+                p_gas = self.p(x)
+                T_coolant[i] = T_coolant[i-1] + (q_dot[i-1]*dx)/self.cooling_jacket.coolant.Cp    #Increase in coolant temperature, q*dx = Cp*dT
+
+                coolant.calculate(T = T_coolant[i], P = self.cooling_jacket.inlet_p0)
+                exhaust_gas.calculate(T = T_gas[i], P = p_gas)
+
+                h_gas = self.h_gas(x, exhaust_gas.mu, exhaust_gas.k, exhaust_gas.Pr)
+                h_coolant = self.h_coolant(x, coolant.mu, coolant.k, coolant.Cp, coolant.rho)
+
+                q_dot[i], R_gas, R_wall, R_coolant = self.thermal_circuit(x, h_gas, h_coolant, self.cooling_jacket.k_wall, T_gas[i], T_coolant[i])
+
+                T_wall_inner[i] = T_gas[i] - q_dot[i]*R_gas
+                T_wall_outer[i] = T_wall_inner[i] - q_dot[i]*R_wall
+            
+        return {"x" : discretised_x,
+                "T_wall_inner" : T_wall_inner,
+                "T_wall_outer" : T_wall_outer,
+                "T_coolant" : T_coolant,
+                "T_gas" : T_gas,
+                "q_dot" : q_dot}
--- cooling_example.py
+++ cooling_example.py
@@ -1,6 +1,7 @@
 import bamboo as bam
 import bamboo.cooling as cool
 import numpy as np
+import matplotlib.pyplot as plt
 import thermo
 import time
 
@@ -11,9 +12,24 @@ molecular_weight = 21.627   #Molecular weight of the exhaust gas (kg/kmol) (only
 '''Chamber conditions'''
 Ac = 0.05           #Chamber cross-sectional area (m^2) - made larger because the original number is smaller than my geometry can deal with right now
 pc = 10e5           #Chamber pressure (Pa)
-Tc = 2458.89        #Chamber temperature (K) - obtained from ProPEP 3
+Tc = 2458.89        #Chamber temperature (K) - obtained from ProPEP 3 with an OF ratio = 3
 mdot = 4.757        #Mass flow rate (kg/s)
 p_amb = 0.4e5       #Ambient pressure (Pa). 1.01325e5 is sea level atmospheric.
+
+thermo_gas = thermo.mixture.Mixture(['N2', 'H2', 'CO','H2O', 'CO2'], zs = [0.368, 0.260, 0.178, 0.0984, 1-0.368-0.260-0.178-0.0984])    #Apprximately from results given by ProPEP 3
+
+'''Engine dimensions'''
+chamber_length = 75e-2
+wall_thickness = 2e-3
+
+'''Coolant jacket'''
+OF_mass_ratio = 3
+mdot_coolant = mdot/(OF_mass_ratio + 1)
+channel_width = 1e-3
+channel_height = 1e-3
+k_wall = 300
+inlet_T = 298.15    #Coolant inlet temperature
+coolant = thermo.chemical.Chemical('Isopropyl Alcohol')
 
 '''Create the engine object'''
 exhaust_gas = bam.Gas(gamma = gamma, molecular_weight = molecular_weight)
@@ -21,15 +37,25 @@ chamber = bam.CombustionChamber(pc, Tc, Ac, mdot)
 nozzle = bam.Nozzle.from_engine_components(exhaust_gas, chamber, p_amb, type = "rao", length_fraction = 0.8)
 white_dwarf = bam.Engine(exhaust_gas, chamber, nozzle)
 
-'''Chemicals used'''
-coolant = thermo.chemical.Chemical('Isopropyl Alcohol')
-
-'''Cooling system geometry'''
-cooling_jacket = cool.CoolingJacket(0.01, 0.01)
-engine_geometry = cool.EngineGeometry(chamber, nozzle, 0.3)
-cooled_engine = cool.EngineWithCooling(engine_geometry, cooling_jacket, coolant, exhaust_gas)
+'''Cooling system setup'''
+cooling_jacket = cool.CoolingJacket(k_wall, channel_width, channel_height, inlet_T, pc, coolant, mdot_coolant)
+engine_geometry = cool.EngineGeometry(chamber, nozzle, chamber_length, wall_thickness)
+cooled_engine = cool.EngineWithCooling(engine_geometry, cooling_jacket, exhaust_gas, thermo_gas)
 
 '''Plots'''
 #engine_geometry.plot_geometry()
-cooled_engine.show_gas_mach()
-cooled_engine.show_gas_temperature()
+#cooled_engine.show_gas_temperature()
+
+'''Run the cooling system simulation'''
+cooling_data = cooled_engine.run_heating_analysis(number_of_points = 1000)
+
+'''Plot the results'''
+plt.plot(cooling_data["x"], cooling_data["T_wall_inner"] - 273.15, label = "Wall (Inner)")
+plt.plot(cooling_data["x"], cooling_data["T_wall_outer"]- 273.15, label = "Wall (Outer)")
+plt.plot(cooling_data["x"], cooling_data["T_coolant"]- 273.15, label = "Coolant")
+#plt.plot(cooling_data["x"], cooling_data["T_gas"], label = "Exhaust gas")
+plt.grid()
+plt.xlabel("Position (m)")
+plt.ylabel("Temperature (deg C)")
+plt.legend()
+plt.show()
