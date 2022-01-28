ca4ec0b0a43bdbbce02349e6ae66dd8f5c6844aa
Daniel Gibbons <61093535+dug20@users.noreply.github.com>
2022-01-28 19:08:56 +0000

    Revert "Added Pavli simulation, and attempted to add momentum pressure loss"
    This reverts commit f8061c1fc780377c4d1903250e4b20605749ee1c.

--- data/vulcain_to_json.py
+++ data/vulcain_to_json.py
@@ -69,7 +69,6 @@ L_positions = np.zeros(len(x_positions))                                        
 wall_thickness = 0.4e-3
 pitch = 1.824
 
-# NOTE I FORGOT TO TAKE INTO ACCOUNT THE SLOPE OF THE CONTOUR, dy/dx. I ONLY TOOK INTO ACCOUNT THE HELIX ANGLE
 for i in range(len(x_positions)):
     if i == 0:
         pass
@@ -103,6 +102,7 @@ for key in output["Nyden"].keys():
     output["Nyden"][key]["x"] = np.interp(x = output["Nyden"][key]["x"], 
                                                xp = L_positions,
                                                fp = x_positions)
+
 
 # Save everything to a .json file
 
