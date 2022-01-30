3e9506b95878031b0f9a6431a84c8b81ac8b3fdf
Daniel Gibbons <61093535+dug20@users.noreply.github.com>
2022-01-30 15:56:41 +0000

    Added temperature data from Pavli

--- data/pavli_to_json.py
+++ data/pavli_to_json.py
@@ -11,6 +11,8 @@ raw["Contour"] = np.loadtxt("Pavli/Pavli_1966_Fig3.csv", delimiter =',', skiprow
 raw["Channel width"] = np.loadtxt("Pavli/Pavli_1966_Fig22.csv", delimiter =',', skiprows = 1)
 raw["Coolant pressure"] = np.loadtxt("Pavli/Pavli_1966_Table2a.csv", delimiter =',', skiprows = 1)
 raw["Coolant temperature"] = np.loadtxt("Pavli/Pavli_1966_Table2b.csv", delimiter =',', skiprows = 1)
+raw["Heat flux"] = np.loadtxt("Pavli/Pavli_1996_Fig14_9.csv", delimiter =',', skiprows = 1)
+raw["Predicted temperatures"] = np.loadtxt("Pavli/Pavli_1996_Fig15.csv", delimiter =',', skiprows = 1)
 
 # Rearrange into a convenient format
 output["Chamber Contour"] = {"x (m)" : raw["Contour"][:,0],
@@ -25,6 +27,15 @@ output["Coolant static pressure"] = {"x (m)" : raw["Coolant pressure"][:,1],
 output["Coolant temperature"] = {"x (m)" : raw["Coolant temperature"][:,1],
                                  "T (K)" : raw["Coolant temperature"][:,2]}
 
+output["Heat flux"] = {"x (m)" : raw["Heat flux"][:,0],
+                       "q (W/m2)" : raw["Heat flux"][:,1]}
+
+output["Predicted temperatures"] = {"x (m)" : raw["Predicted temperatures"][:,0],
+                                    "Tc (K)" : raw["Predicted temperatures"][:,1],
+                                    "Twc (K)" : raw["Predicted temperatures"][:,2],
+                                    "Twh1 (K)" : raw["Predicted temperatures"][:,3],
+                                    "Twh2 (K)" : raw["Predicted temperatures"][:,4],
+                                    "Th (K)" : raw["Predicted temperatures"][:,5]}
 
 # Save everything to a .json file
 # Encoder to make numpy arrays .json serializable https://stackoverflow.com/questions/26646362/numpy-array-is-not-json-serializable
